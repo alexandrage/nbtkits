89e5c70256812cc3eb98fe49e32b0a838fbf8b98
alexandrage <alexandrage@mail.ru>
2017-01-30 20:23:12 +0300

    nbtkits 2.0
    Переписан InventoryNBTSer, не совместимо со старой версией.

--- nbtkits/src/nbtkits/InventoryNBTSer.java
+++ nbtkits/src/nbtkits/InventoryNBTSer.java
@@ -1,201 +1,104 @@
 package nbtkits;
-
-
-import net.minecraft.server.v1_7_R4.NBTCompressedStreamTools;
-import net.minecraft.server.v1_7_R4.NBTTagCompound;
-import net.minecraft.server.v1_7_R4.NBTTagList;
-
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.craftbukkit.v1_7_R4.inventory.CraftInventoryCustom;
-import org.bukkit.craftbukkit.v1_7_R4.inventory.CraftItemStack;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-
-import com.google.common.io.Files;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map.Entry;
 
-public class InventoryNBTSer {	
-	
-	public static void getKit(Player p, File folder, String name, boolean b) throws IOException {
-		if(!new File(folder+"/kits/"+name.toLowerCase()+".kit").exists()) {
-			p.sendMessage("§4Такого набора нет!");
-			p.playSound(p.getLocation(), Sound.EXPLODE, 1, 2);
-			return;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class kits extends JavaPlugin {
+	File folder = getDataFolder();
+	public void onEnable() {
+		getConfig().options().copyDefaults(true);
+		getDataFolder().mkdirs();
+		folder.mkdir();
+		PluginManager pm = getServer().getPluginManager();
+		death d = new death(this);
+		pm.registerEvents(d, this);
+		getLogger().info("kits Enabled!");
 		}
-    	
-		byte[] bytes = Files.toByteArray(new File(folder+"/kits/"+name.toLowerCase()+".kit"));
-		String[] full = new String(bytes).split("-");
-		String time = full[1];
-		String Inv  = full[0];
-		
-		if(!b) {
-			if(new File(folder+"/players/"+name.toLowerCase()+"-"+p.getName()).exists()) {
-				byte[] bytesp = Files.toByteArray(new File(folder+"/players/"+name.toLowerCase()+"-"+p.getName()));
-				String tpl = new String(bytesp);
-				long  calc = (System.currentTimeMillis()-(Long.parseLong(tpl)))/1000;
-				Time t = new Time(Long.parseLong((time))-calc);
-				if(calc<Long.parseLong((time))) {
-					p.sendMessage("§4Вы не можете получить этот набор, раньше чем через §c"+t.getFormat());
-					p.playSound(p.getLocation(), Sound.ANVIL_BREAK, 1, 0);
-					return;
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("kit") && sender instanceof Player) {
+			try {
+				if (args.length == 3) {
+					if(sender.hasPermission("kits.admin") && args[0].matches("^[A-Za-z0-9]{1,10}$") && args[1].matches("^[A-Za-z0-9]{1,10}$") && args[2].matches("^[0-9]{1,32}$")) {
+						long time = Long.parseLong(args[2]);
+						if(args[0].equalsIgnoreCase("set") && !args[1].equalsIgnoreCase("time")) {
+							InventoryNBTSer.setKit((Player) sender, folder, args[1], time);
+							return true;
+						}
+						if(args[0].equalsIgnoreCase("time")) {
+							InventoryNBTSer.setTime((Player) sender, folder, args[1], time);
+							return true;
+						}
+					}
+				}
+				if (args.length == 2) {
+					if(sender.hasPermission("kits.admin") && args[0].matches("^[A-Za-z0-9]{1,10}$") && args[1].matches("^[A-Za-z0-9_]{1,10}$")) {
+						if(args[0].equalsIgnoreCase("set") && !args[1].equalsIgnoreCase("time")) {
+							InventoryNBTSer.setKit((Player) sender, folder, args[1], 86400);
+							return true;
+						}
+						if(args[0].equalsIgnoreCase("del")) {
+							InventoryNBTSer.delKit(folder, args[1], (Player) sender);
+							return true;
+						}
+						Player pl = Bukkit.getPlayer(args[1]);
+						if(pl==null) {
+							sender.sendMessage("§4Игрок не найден");
+							((Player) sender).playSound(((Entity) sender).getLocation(), Sound.EXPLODE, 1, 2);
+							return true;
+						}
+						InventoryNBTSer.getKit(pl, folder, args[0], true);
+						return true;
+					}
+				}
+				if (args.length == 1) {
+					if(sender.hasPermission("kits.kit."+args[0]) && args[0].matches("^[A-Za-z0-9]{1,10}$")) {
+						InventoryNBTSer.getKit((Player) sender, folder, args[0], sender.hasPermission("kits.bypass"));
+						return true;
+					} else {
+						sender.sendMessage("§4У вас нет разрешения на этот набор!");
+						Player p = (Player) sender;
+						p.playSound((p).getLocation(), Sound.EXPLODE, 1, 2);
+						return true;
+					}
+				}
+				if (args.length == 0) {
+					List<String> l = new ArrayList<String>();
+					for(String s : InventoryNBTSer.getLogs(folder)) {
+						if(sender.hasPermission("kits.kit."+s)) {
+							l.add(s);
+						}
+					}
+					sender.sendMessage("§6Наборы §f"+l.toString().substring(1, l.toString().length()-1).replace(",", ""));
+					Player p = (Player) sender;
+					p.playSound((p).getLocation(), Sound.CHEST_OPEN, 1, 1);
+					return true;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (cmd.getName().equalsIgnoreCase("kits")) {
+			List<String> l = new ArrayList<String>();
+			for(String s : InventoryNBTSer.getLogs(folder)) {
+				if(sender.hasPermission("kits.kit."+s)) {
+					l.add(s);
 				}
 			}
+			sender.sendMessage("§6Наборы §f"+l.toString().substring(1, l.toString().length()-1).replace(",", ""));
+			Player p = (Player) sender;
+			p.playSound((p).getLocation(), Sound.CHEST_OPEN, 1.0F, 1.0F);
+			return true;
 		}
-	
-		ItemStack[] st = fromString(Inv).getContents();
-		for(ItemStack s : st) {
-			if(s != null) {
-				HashMap<Integer, ItemStack> over = p.getInventory().addItem(s);
-				for(Entry<Integer, ItemStack> entry : over.entrySet()) {   
-				    p.getWorld().dropItemNaturally(p.getLocation(), entry.getValue());
-				}
-			}
-		}
-		p.sendMessage("§6Получен набор §c"+name.toLowerCase());
-		p.playSound(p.getLocation(), Sound.ANVIL_LAND, 1, 0);
-		new File(folder+"/players/").mkdirs();
-		Files.write(Long.toString(System.currentTimeMillis()).getBytes(), new File(folder+"/players/"+name.toLowerCase()+"-"+p.getName()));
+		return false;
 	}
-	
-	public static void setKit(Player p, File folder, String name, long time) throws IOException {
-		new File(folder+"/kits").mkdirs(); 
-		String Inv = toString(p.getInventory().getContents());
-		String full = Inv+"-"+time;
-		byte[] bytes = full.getBytes();
-		Files.write(bytes, new File(folder+"/kits/"+name.toLowerCase()+".kit"));
-		p.sendMessage("§2Набор сохранен.");
-		p.playSound(p.getLocation(), Sound.ANVIL_BREAK, 1, 1);
-	}
-	
-	public static void setTime(Player p, File folder, String name, long time) throws IOException {
-		if(!new File(folder+"/kits/"+name.toLowerCase()+".kit").exists()) {
-			p.sendMessage("§4Такого набора нет!");
-			p.playSound(p.getLocation(), Sound.EXPLODE, 1, 2);
-			return;
-		}
-		byte[] bytes = Files.toByteArray(new File(folder+"/kits/"+name.toLowerCase()+".kit"));
-		String[] full = new String(bytes).split("-");
-		String Inv  = full[0];
-		String full2 = Inv+"-"+time;
-		byte[] bytes2 = full2.getBytes();
-		Files.write(bytes2, new File(folder+"/kits/"+name.toLowerCase()+".kit"));
-		p.sendMessage("§2Время изменено.");
-		p.playSound(p.getLocation(), Sound.ANVIL_BREAK, 1, 1);
-	}
-	
-    private static String toString(ItemStack[] inventory) {
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        NBTTagList itemList = new NBTTagList();
-        for (int i = 0; i < inventory.length; i++) {
-            NBTTagCompound outputObject = new NBTTagCompound();
-            boolean b = false;
-            if(inventory[i] != null) {
-            	b = inventory[i].getType() == Material.AIR;
-            }
-            if(b) {
-            	inventory[i] = null;
-            }
-            CraftItemStack craft = getCraftVersion(inventory[i]);
-            if (craft != null)
-                CraftItemStack.asNMSCopy(craft).save(outputObject);
-
-            itemList.add(outputObject);
-        }
-
-        NBTTagCompound whole = new NBTTagCompound();
-        whole.set("Inventory", itemList);
-        NBTCompressedStreamTools.a(whole, outputStream);
-
-        return new BigInteger(1, outputStream.toByteArray()).toString(32);
-    }
-
-    private static Inventory fromString(String data) {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(new BigInteger(data, 32).toByteArray());
-        NBTTagList itemList = NBTCompressedStreamTools.a(inputStream).getList("Inventory", 10);
-        Inventory inventory = new CraftInventoryCustom(null, itemList.size());
-
-        for (int i = 0; i < itemList.size(); i++) {
-            NBTTagCompound inputObject = itemList.get(i);
-            if (!inputObject.isEmpty()) {
-                inventory.setItem(i, CraftItemStack.asBukkitCopy(net.minecraft.server.v1_7_R4.ItemStack.createStack(inputObject)));
-            }
-        }
-        return inventory;
-    }
-
-    private static CraftItemStack getCraftVersion(ItemStack stack) {
-        if (stack instanceof CraftItemStack)
-            return (CraftItemStack) stack;
-        else if (stack != null)
-            return CraftItemStack.asCraftCopy(stack);
-        else
-            return null;
-    }
-
-	public static void delKit(File folder, String name, Player p) {
-		File k = new File(folder+"/kits/"+name.toLowerCase()+".kit");
-		if(k.exists()) {
-			k.delete();
-			p.sendMessage("§2Набор удален.");
-			p.playSound(p.getLocation(), Sound.ANVIL_BREAK, 1, 1);
-		} else {
-			p.sendMessage("§4Такого набора нет!");
-			p.playSound(p.getLocation(), Sound.EXPLODE, 1, 0);
-		}
-	}
-	
-	public static List<String> getLogs(File folder) {
-		List<String> l = new ArrayList<String>();
-		File Logs = new File(folder+"/kits"); 
-		Logs.mkdirs();
-		for (File file : Logs.listFiles()) {
-			String kit = file.toString().substring(file.toString().lastIndexOf(File.separator)+1);
-			if(kit.contains(".kit")) {
-				l.add(kit.replace(".kit", ""));
-			}
-		}
-		return l;
-	}
-	  
-	public static void setInv(Player p, File folder) throws IOException {
-		File folderinv = new File(folder+"/saveinv"); 
-		folderinv.mkdirs();
-		String Arm = toString(p.getInventory().getArmorContents());
-		String Inv = toString(p.getInventory().getContents());
-		String full = Arm+"-"+Inv;
-		byte[] bytes = full.getBytes();
-		Files.write(bytes, new File(folderinv+"/"+p.getName()));
-		p.getInventory().clear();
-		ItemStack[] in = p.getInventory().getArmorContents();
-		for (int i = 0; i < in.length; i++) {
-			in[i] = new ItemStack(Material.AIR, 0);
-		}
-		p.getInventory().setArmorContents(in);  
-	}
-		
-	public static void getInv(Player p, File folder) throws IOException {
-		File folderinv = new File(folder+"/saveinv"); 
-		folderinv.mkdirs();
-		if(!new File(folderinv+"/"+p.getName()).exists()) {
-			return;
-		}
-		byte[] bytes = Files.toByteArray(new File(folderinv+"/"+p.getName()));
-		String full = new String(bytes);
-		String[] spl = full.split("-");
-		new File(folderinv+"/"+p.getName()).delete();
-		p.getInventory().setArmorContents(fromString(spl[0]).getContents());
-		p.getInventory().setContents(fromString(spl[1]).getContents());
-	}  
 }
